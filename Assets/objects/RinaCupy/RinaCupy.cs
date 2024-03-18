6c8f1c90905999307dac2af03637d9321d4e8910
masarina <50221377+masarina@users.noreply.github.com>
2024-03-19 03:26:18 +0900

    Update RinaCupy.cs

--- Assets/objects/RinaCupy/RinaCupy.cs
+++ Assets/objects/RinaCupy/RinaCupy.cs
@@ -1,42 +1,27 @@
-using UdonSharp;
-using UnityEngine;
+    public ComputeShader subtractArrayComputeShader; // 上で作成したComputeShader
 
-public class RinaCupy : UdonSharpBehaviour
-{
-    public ComputeShader sumArrayComputeShader; // Sum_FloatArray計算がHLSLで書かれたCompute Shaderをアタッチ。
-
-    public float Sum_FloatArray_GPU(float[] x) {
-        // Texture2Dの作成
+    public float[] Subtract_FloatArray_Float_GPU(float[] x, float subtractValue) {
         Texture2D inputTexture = new Texture2D(x.Length, 1, TextureFormat.RFloat, false);
-        // データをTextureに設定
         for (int i = 0; i < x.Length; i++) {
             inputTexture.SetPixel(i, 0, new Color(x[i], 0, 0, 0));
         }
-        inputTexture.Apply(); // データのアップロードを確定
+        inputTexture.Apply();
 
-        // Compute Shaderのセットアップ
-        int kernelHandle = sumArrayComputeShader.FindKernel("CSMain");
-        ComputeBuffer resultBuffer = new ComputeBuffer(1, sizeof(float), ComputeBufferType.Default);
-        sumArrayComputeShader.SetTexture(kernelHandle, "InputArray", inputTexture);
-        sumArrayComputeShader.SetBuffer(kernelHandle, "Result", resultBuffer);
-        
-                
-        // Compute Shaderの実行
-        int dataCount = x.Length; // xは入力データの配列
-        int threadsPerGroup = 1024; // Compute Shaderで設定した[numthreads(1024, 1, 1)]のX値
-        int groupCount = Mathf.CeilToInt((float)dataCount / threadsPerGroup); // 必要なグループ数を計算
-        
-        sumArrayComputeShader.Dispatch(kernelHandle, groupCount, 1, 1); // 必要なグループ数でDispatchを呼び出し
-                
-                
-        // 結果の取得
-        float[] result = new float[1];
+        int kernelHandle = subtractArrayComputeShader.FindKernel("CSMain");
+        ComputeBuffer resultBuffer = new ComputeBuffer(x.Length, sizeof(float));
+        subtractArrayComputeShader.SetTexture(kernelHandle, "InputArray", inputTexture);
+        subtractArrayComputeShader.SetBuffer(kernelHandle, "Result", resultBuffer);
+        subtractArrayComputeShader.SetFloat("SubtractValue", subtractValue);
+
+        int threadsPerGroup = 1024;
+        int groupCount = Mathf.CeilToInt((float)x.Length / threadsPerGroup);
+        subtractArrayComputeShader.Dispatch(kernelHandle, groupCount, 1, 1);
+
+        float[] result = new float[x.Length];
         resultBuffer.GetData(result);
-        resultBuffer.Release(); // リソースの解放
+        resultBuffer.Release();
 
-        // Texture2Dのリソースも解放
         Destroy(inputTexture);
 
-        return result[0];
+        return result;
     }
-}
