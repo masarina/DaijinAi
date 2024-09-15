3ac18ae61a2c13c99c0c789521629a1c87c99e51
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-16 07:18:21 +0900

    ParamsSettingPlayer.py を更新

--- ServerSide/BallPassSchedulePattern/Players_ParamsSettingTyps/ParamsSettingPlayerDir/ParamsSettingPlayer.py
+++ ServerSide/BallPassSchedulePattern/Players_ParamsSettingTyps/ParamsSettingPlayerDir/ParamsSettingPlayer.py
@@ -6,19 +6,38 @@ class ParamsSettingPlayer(SuperPlayer):
     def __init__(self):
         super().__init__()
         self.my_name = None
+        self.model_mode = None
 
     def return_my_name(self):
         return "ParamsSettingPlayer"
 
     def main(self):
         """
-        モードに応じてエンコードまたはデコードを実行し、結果をメンバ変数に保持
+        各プレイヤーの初期化。
         """
+        
+        """
+        入力データの初期化
+        """
+        # 入力データのバッチ化
+        
+        
+        xs_data_tokenType = self.one_time_world_instance.xsDataPlayer.xs_data_tokenType # 二次元トークンデータを取得
+        vocabularyBuilderPlayer = self.one_time_world_instance.vocabularyBuilderPlayer # ID辞書変換機を取得
+        vocabularyBuilderPlayer.mode = "vocab_to_id" # ID辞書変換器のモードを設定
+        xs_data_idType = vocabularyBuilderPlayer.
+        
+        
         if self.model_mode == "train":
-            pass
+            
+            
+            """
+            EmbeddingLayerの初期化
+            """
+            self.one_time_world_instance.embeddingPlayer.
+            
         elif self.model_mode == "predict":
             pass
             
             
         return "Completed"
-
