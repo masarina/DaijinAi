0847c5817037a4ad179f6a316bfeada1e75e3c31
Masarina <50221377+masarina@users.noreply.github.com>
2024-10-03 01:23:49 +0900

    ColumnSplitterConcatPlayer.py を更新

--- ServerSide/BallPassSchedulePattern/Players_QrcodeReaderTypes/Decode_c_ColumnSplitterConcatPlayerDir/ColumnSplitterConcatPlayer.py
+++ ServerSide/BallPassSchedulePattern/Players_QrcodeReaderTypes/Decode_c_ColumnSplitterConcatPlayerDir/ColumnSplitterConcatPlayer.py
@@ -23,12 +23,6 @@ class ColumnSplitterConcatPlayer(SuperPlayer):
         このメソッド実行直前に、スーパークラスのメンバ変数
         one_time_world_instanceに、最新のworldインスタンスを代入しています。
         """
-        # DataFrameの右から列を分割して縦に結合する処理を行う
-        df = self.one_time_world_instance.some_dataframe  # 任意のDataFrameを取得
-        num_columns_to_split = 2  # 例えば右から2列を分割する
-        split_df = df.iloc[:, -num_columns_to_split:]  # 右から列を取得
-        remaining_df = df.iloc[:, :-num_columns_to_split]  # 残りの列
-        concatenated_df = pd.concat([remaining_df, split_df], axis=0)  # 縦に結合
 
         # 結果を保存
         self.one_time_world_instance.processed_dataframe = concatenated_df
