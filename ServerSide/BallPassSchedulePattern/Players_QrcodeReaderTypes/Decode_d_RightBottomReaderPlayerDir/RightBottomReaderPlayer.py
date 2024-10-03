d361e33ef4a3b988c2dfc9d9205391f000709377
Masarina <50221377+masarina@users.noreply.github.com>
2024-10-03 23:22:43 +0900

    RightBottomReaderPlayer.py を更新

--- ServerSide/BallPassSchedulePattern/Players_QrcodeReaderTypes/Decode_d_RightBottomReaderPlayerDir/RightBottomReaderPlayer.py
+++ ServerSide/BallPassSchedulePattern/Players_QrcodeReaderTypes/Decode_d_RightBottomReaderPlayerDir/RightBottomReaderPlayer.py
@@ -15,8 +15,11 @@ class RightBottomReaderPlayer(SuperPlayer):  # 名前はりなに決めてもら
     
     def flatten_list2d(self, list2d=None):
         """ 2次元リストを1次元リストにするメソッド
-        ・shape的に(n, 2)の2次元リストに限る
+        ・QRcodeに特化。shape的に(n, 2)の2次元リストに限る
         """
+        
+        """ 入力 """
+        list2d
         
         """ 初期化 """
         read_list = [] # 読み込まれた1次元リスト(最終的に完成品となる)
@@ -36,8 +39,9 @@ class RightBottomReaderPlayer(SuperPlayer):  # 名前はりなに決めてもら
                 read_list.append(pair[0]):
             else:
                 pass
-                
-            
+        
+        """ 出力 """
+        return read_list
     
 
     def main(self):
@@ -46,16 +50,21 @@ class RightBottomReaderPlayer(SuperPlayer):  # 名前はりなに決めてもら
         このスーパークラスのメンバ変数 one_time_world_instanceに、
         最新のworldインスタンスを代入しています。
         """
+        
+        """ 初期化 """
         # QRコードのマトリックスを取得
         binary_matrix = self.one_time_world_instance.qrCodeCorrectionPlayer.binary_matrix_2Dlist
-
+        
+        """ メイン処理 """
         # データを右下から読み込む
-        self.data_read = self.read_data_from_right_bottom(binary_matrix)
-
+        self.data_read = self.flatten_list2d(list2d=binary_matrix)
         # 読み込んだデータを確認
         print(f"データが右下から読み込まれました: {self.data_read}")
-
         # 自身を更新
         self.one_time_world_instance.rightBottomReaderPlayer = self
+
+        """ 完成したデータ """
+        self.data_read
+
 
         return "Completed"
