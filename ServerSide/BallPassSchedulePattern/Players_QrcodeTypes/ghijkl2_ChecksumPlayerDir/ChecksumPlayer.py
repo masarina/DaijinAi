59e4d15a5a20a54dd4c9ce6e2e6b905c384bf7e5
Masarina <50221377+masarina@users.noreply.github.com>
2024-10-19 02:45:52 +0900

    ChecksumPlayer.py を更新

--- ServerSide/BallPassSchedulePattern/Players_QrcodeTypes/ghijkl2_ChecksumPlayerDir/ChecksumPlayer.py
+++ ServerSide/BallPassSchedulePattern/Players_QrcodeTypes/ghijkl2_ChecksumPlayerDir/ChecksumPlayer.py
@@ -6,6 +6,7 @@ class ChecksumPlayer(SuperPlayer):
     def __init__(self):
         super().__init__()  # スーパークラスの初期化メソッドを呼び出す
         self.my_name = None  # 必ずNoneで初期化
+        self.mode_charNumInfo_data_pad4_pad8_checksum_decimal_list
 
     def return_my_name(self):
         return "ChecksumPlayer"
@@ -39,17 +40,21 @@ class ChecksumPlayer(SuperPlayer):
         """
         mainメソッドで全てのメソッドを実行
         """
+        
+        """ 入力 """
+        self.data_str = woT.data_bits # データのみ
+        self.mode_charNumInfo_data_pad4_pad8_list = mode_charNumInfo_data_pad4_pad8_list
+        self.loop11101100and00010001pad_only_list = loop11101100and00010001pad_only_list
+        to_decimal = self.one_time_world_instance.polynomialDivisionPlayer.bit_list_to_decimal_list
+        
         # 任意のデータリスト
-        data_list = [100, 200, 50, 75, 25]
+        mode_charNumInfo_data_pad4_pad8_decimal_list = to_decimal(self.mode_charNumInfo_data_pad4_pad8_list)
         
         # Checksumの計算
-        checksum = self.calculate_checksum(data_list)
+        checksum = self.calculate_checksum(mode_charNumInfo_data_pad4_pad8_decimal_list)
         
         # Checksumをデータに追加
-        data_list_with_checksum = self.append_checksum(data_list, checksum)
-        
-        # データリストとChecksumの出力
-        self.print_data(data_list, checksum, data_list_with_checksum)
+        self.mode_charNumInfo_data_pad4_pad8_checksum_decimal_list = self.append_checksum(mode_charNumInfo_data_pad4_pad8_decimal_list, checksum)
         
         # プレイヤー自身を更新
         self.one_time_world_instance.checksumPlayer = self
