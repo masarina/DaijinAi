63ac0c34c87b61589d1adef1b183346f48e09b20
Masarina <50221377+masarina@users.noreply.github.com>
2024-10-25 04:23:08 +0900

    SpiritBitDataPlayer.py を更新

--- ServerSide/BallPassSchedulePattern/Players_QrcodeReaderTypes/Decode_g_SpiritBitDataPlayerDir/SpiritBitDataPlayer.py
+++ ServerSide/BallPassSchedulePattern/Players_QrcodeReaderTypes/Decode_g_SpiritBitDataPlayerDir/SpiritBitDataPlayer.py
@@ -7,6 +7,11 @@ class SpiritBitDataPlayer(SuperPlayer):
     def __init__(self):
         super().__init__()  # スーパークラスの初期化メソッドを呼び出す
         self.my_name = None  # 必ずNoneで初期化
+        self.png_file_path # 写真のパス
+        self.processed_data # 8bit毎にスプリットしたデータ
+        self.mode = None
+        self.charNum_info = None
+        self.data = None
 
     def return_my_name(self):
         return "SpiritBitDataPlayer"
@@ -18,8 +23,17 @@ class SpiritBitDataPlayer(SuperPlayer):
         
         このプレイヤーはSpiritBitに関連するデータを操作する予定です。
         """
-        # データ操作の処理を書く場所
-        print(f"{self.return_my_name()}が実行されました。")
+        """ 入力 """
+        woP = self.one_time_world_instance.checksumCheekPlayer
+        self.png_file_path = woP.png_file_path # 写真のパス
+        self.processed_data = woP.processed_data # 8bit毎にスプリットしたデータ
+        
+        """ メイン """
+        # modeの摘出
+        
+        # 文字数情報の摘出
+        
+        # データの摘出
 
         # プレイヤー自身を更新
         self.one_time_world_instance.spiritBitDataPlayer = self
