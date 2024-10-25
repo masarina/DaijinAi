b8033b7d11058661e594d773b877a42df8868798
Masarina <50221377+masarina@users.noreply.github.com>
2024-10-25 23:42:46 +0900

    SpiritBitDataPlayer.py を更新

--- ServerSide/BallPassSchedulePattern/Players_QrcodeReaderTypes/Decode_g_SpiritBitDataPlayerDir/SpiritBitDataPlayer.py
+++ ServerSide/BallPassSchedulePattern/Players_QrcodeReaderTypes/Decode_g_SpiritBitDataPlayerDir/SpiritBitDataPlayer.py
@@ -12,6 +12,7 @@ class SpiritBitDataPlayer(SuperPlayer):
         self.mode = None
         self.charNum_info = None
         self.data = None
+        self.charNumInfo_decimal
 
     def return_my_name(self):
         return "SpiritBitDataPlayer"
