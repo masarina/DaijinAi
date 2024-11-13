e67cef346d66e70bb8e7b3518b14306ca33322fc
Masarina <50221377+masarina@users.noreply.github.com>
2024-11-14 05:07:04 +0900

    ChecksumPlayer.cs を更新

--- InstanceSide/BallPassSchedulePattern/Players_QRcodeTypes/efghijkl_ChecksumPlayerDir/ChecksumPlayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_QRcodeTypes/efghijkl_ChecksumPlayerDir/ChecksumPlayer.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class ChecksumPlayer : SuperPlayer
 {
-    public RinaNumpy rinaNumpy;  // RinaNumpyクラスを使用
-    public string[] modeCharNumInfoChecksumBitList;
+    public RinaNumpy rinaNumpy;  // RinaNumpyクラスのインスタンス
     public QRCodeBitConversionPlayer qRCodeBitConversionPlayer;
     public PolynomialDivisionPlayer polynomialDivisionPlayer;
+    
+    public string[] modeCharNumInfoChecksumBitlist;
+    public string dataStr;
+    public string modeCharNumInfoBitlist;
 
     public override string ReturnMyName()
     {
@@ -15,41 +18,74 @@ public class ChecksumPlayer : SuperPlayer
 
     public int CalculateChecksum(int[] dataList)
     {
-        // データリストからチェックサムを計算するメソッド
+        // データリストからChecksumを計算し、256で割った余りを返す
         int sum = 0;
-        foreach (int value in dataList)
+        for (int i = 0; i < dataList.Length; i++)
         {
-            sum += value;
+            sum += dataList[i];
         }
-        return sum % 256;  // チェックサムを256で割った余り
+        return sum % 256;
     }
 
     public int[] AppendChecksum(int[] dataList, int checksum)
     {
-        // データリストにチェックサムを追加
-        int[] result = new int[dataList.Length + 1];
-        dataList.CopyTo(result, 0);
-        result[dataList.Length] = checksum;
+        // データリストにChecksumを追加
+        int[] dataListWithChecksum = new int[dataList.Length + 1];
+        for (int i = 0; i < dataList.Length; i++)
+        {
+            dataListWithChecksum[i] = dataList[i];
+        }
+        dataListWithChecksum[dataList.Length] = checksum;
+        return dataListWithChecksum;
+    }
+
+    private string ArrayToString(int[] array)
+    {
+        // 配列を文字列に変換
+        string result = "";
+        for (int i = 0; i < array.Length; i++)
+        {
+            result += array[i].ToString();
+            if (i < array.Length - 1)
+            {
+                result += ", ";
+            }
+        }
         return result;
+    }
+
+    public void PrintData(int[] dataList, int checksum, int[] dataListWithChecksum)
+    {
+        // データリスト、Checksum、Checksum付きデータリストをログ出力
+        Debug.Log("元のデータリスト: " + ArrayToString(dataList));
+        Debug.Log("Checksum: " + checksum);
+        Debug.Log("Checksum付きデータリスト: " + ArrayToString(dataListWithChecksum));
     }
 
     public override string ExecuteMain()
     {
-        // ワールドインスタンスからデータを取得
-        string dataStr = this.qRCodeBitConversionPlayer.dataBits;
-        string modeAndCountInfoBitStr = this.qRCodeBitConversionPlayer.modeAndCountInfoBit;
+        // QRCodeBitConversionPlayerからビットデータを取得
+        this.dataStr = qRCodeBitConversionPlayer.dataBits;
+        this.modeCharNumInfoBitlist = qRCodeBitConversionPlayer.modeAndCountInfoBit + this.dataStr;
+
+        // PolynomialDivisionPlayerからビット列を10進数リストに変換
+        int[] modeCharNumInfoDecimalList = polynomialDivisionPlayer.BitListToDecimalList(this.modeCharNumInfoBitlist);
+
+        // Checksumの計算
+        int checksum = CalculateChecksum(modeCharNumInfoDecimalList);
+
+        // Checksumをデータリストに追加
+        int[] modeCharNumInfoChecksumBitlist = AppendChecksum(modeCharNumInfoDecimalList, checksum);
+        this.modeCharNumInfoChecksumBitlist = new string[modeCharNumInfoChecksumBitlist.Length];
         
-        // モードと文字数情報ビットリストを結合
-        string modeCharNumInfoBitList = modeAndCountInfoBitStr + dataStr;
+        // 配列の内容を文字列形式で保存
+        for (int i = 0; i < modeCharNumInfoChecksumBitlist.Length; i++)
+        {
+            this.modeCharNumInfoChecksumBitlist[i] = modeCharNumInfoChecksumBitlist[i].ToString();
+        }
 
-        // ビットリストを10進数リストに変換
-        int[] modeCharNumInfoDecimalList = polynomialDivisionPlayer.BitListToDecimalList(modeCharNumInfoBitList);
-
-        // チェックサムの計算
-        int checksum = this.CalculateChecksum(modeCharNumInfoDecimalList);
-
-        // チェックサムをデータに追加
-        modeCharNumInfoChecksumBitList = AppendChecksum(modeCharNumInfoDecimalList, checksum);
+        // 自身のインスタンスをone_time_world_instanceに登録
+        oneTimeWorldInstance.checksumPlayer = this;
 
         return "Completed";
     }
