bcad4bf59996512b67bc17d9cebff7346c439557
Masarina <50221377+masarina@users.noreply.github.com>
2024-11-29 03:25:51 +0900

    NormalizationLayer.cs を更新

--- InstanceSide/BallPassSchedulePattern/Players_AiTypes/NormalizationPlayerDir/NormalizationLayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/NormalizationPlayerDir/NormalizationLayer.cs
@@ -1,7 +1,7 @@
 using UdonSharp;
 using UnityEngine;
 
-public class LayerNormalizationSingle : UdonSharpBehaviour
+public class NormalizationLayer : UdonSharpBehaviour
 {
     public RinaNumpy rNp; // InspectorからRinaNumpyオブジェクトをアサイン
 
