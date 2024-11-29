9bfbff7b58c7a02f538010657f77be0b6faa4488
Masarina <50221377+masarina@users.noreply.github.com>
2024-11-29 22:45:48 +0900

    Update LiON.py

--- ServerSide/BallPassSchedulePattern/Players_AiTypes/UpdatePlayerDir/LiON.py
+++ ServerSide/BallPassSchedulePattern/Players_AiTypes/UpdatePlayerDir/LiON.py
@@ -1,14 +1,18 @@
 import numpy as np
 
-class LiON:
-    def __init__(self, alpha=0.01, beta=0.9):
+class LiONWithScaling:
+    def __init__(self, alpha=0.01, beta=0.9, threshold=1.0, scaling_factor=0.9):
         """
-        LiONアップデートの初期化
+        スケールダウンを組み込んだLiONアップデート
         :param alpha: 学習率 (デフォルト: 0.01)
         :param beta: モーメント項の減衰率 (デフォルト: 0.9)
+        :param threshold: スケールダウンのしきい値 (デフォルト: 1.0)
+        :param scaling_factor: スケールダウン係数 (デフォルト: 0.9)
         """
         self.alpha = alpha  # 学習率
         self.beta = beta    # モーメント係数
+        self.threshold = threshold  # スケールダウンのしきい値
+        self.scaling_factor = scaling_factor  # スケールダウン係数
         self.v = None       # モーメントベクトル
 
     def initialize(self, parameter_size):
@@ -20,7 +24,7 @@ class LiON:
 
     def update_weights(self, w, dw):
         """
-        LiONによる重み更新
+        LiONによる重み更新（スケールダウンを考慮）
         :param w: 現在の重み (NumPy配列)
         :param dw: 重みに対する勾配 (NumPy配列)
         :return: 更新された重み (NumPy配列)
@@ -31,7 +35,15 @@ class LiON:
         # モーメントの更新
         self.v = self.beta * self.v + (1 - self.beta) * dw
 
-        # 重みの更新
+        # スケールダウン: モーメントがしきい値を超えた場合
+        mask_v = np.abs(self.v) > self.threshold
+        self.v[mask_v] *= self.scaling_factor
+
+        # 重みを更新
         w -= self.alpha * np.sign(self.v)
+
+        # スケールダウン: 重みがしきい値を超えた場合
+        mask_w = np.abs(w) > self.threshold
+        w[mask_w] *= self.scaling_factor
 
         return w
