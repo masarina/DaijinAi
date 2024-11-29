ee7b0e26480835f116454a5f471bc719f770759c
Masarina <50221377+masarina@users.noreply.github.com>
2024-11-30 03:31:47 +0900

    AttentionWeightLayer.cs を更新

--- InstanceSide/BallPassSchedulePattern/Players_AiTypes/AttentionWeightPlayerDir/AttentionWeightLayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/AttentionWeightPlayerDir/AttentionWeightLayer.cs
@@ -1,7 +1,7 @@
 using UdonSharp;
 using UnityEngine;
 
-public class AttentionWeight : UdonSharpBehaviour
+public class AttentionWeightLayer : UdonSharpBehaviour
 {
     public SoftmaxLayer softmaxLayer; // SoftmaxLayerをインスペクタからアタッチ
     public RinaNumpy rNp; // RinaNumpyをインスペクタからアタッチ
