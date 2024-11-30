b3e9db9dd4e475596f01d9e031b17f794a20a7bd
Masarina <50221377+masarina@users.noreply.github.com>
2024-11-30 21:06:25 +0900

    Create EmbeddingPlayer.cs

---
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/EmbeddingPlayerDir/EmbeddingPlayer.cs
@@ -0,0 +1,43 @@
+using UdonSharp;
+using UnityEngine;
+
+public class AffinePlayer : SuperPlayer
+{
+    public AffineLayer affineLayer;
+    public InitAiTypesPlayer initAiTypesPlayer;
+    
+    // 初期化メソッド (Pythonの__init__に相当)
+    public bool AffinePlayerReset()
+    {
+        myName = "AffinePlayer";
+        return true;
+    }
+
+    // プレイヤーの名前を返すメソッド
+    public override string ReturnMyName()
+    {
+        return "AffinePlayer";
+    }
+
+    public float[] Forward(float[] x)
+    {
+        y = affineLayer.Forward(x);
+        
+        return y;
+    }
+
+    public float[] Backward(float[] dx)
+    {
+        dout = affineLayer.Backward(dx)
+
+        return dout;
+    }
+    
+    // メイン処理を行うメソッド
+    public override string ExecuteMain()
+    {
+        // ForwardPlayerでライブラリ的に使用するので
+        // 実装今のところ不要
+        return "Completed";
+    }
+}
