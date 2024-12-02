756ec3ba1bc02d7202c5ab4a02565f95a021931b
Masarina <50221377+masarina@users.noreply.github.com>
2024-12-03 00:06:21 +0900

    Create ParamsPlayer.cs

---
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/ParamsPlayerDir/ParamsPlayer.cs
@@ -0,0 +1,32 @@
+using UdonSharp;
+using UnityEngine;
+
+public class ParamsPlayer : SuperPlayer
+{
+    public RinaNumpy rinaNumpy;
+    public string myName;
+    public InitAiTypesPlayer initAiTypesPlayer;
+    public float[][][][] = rinaNumpy.
+    
+    // 初期化メソッド (Pythonの__init__に相当)
+    public bool AffinePlayerReset()
+    {
+        myName = "AffinePlayer";
+        return true;
+    }
+
+    // プレイヤーの名前を返すメソッド
+    public override string ReturnMyName()
+    {
+        return "ParamsPlayer";
+    }
+
+    
+    // メイン処理を行うメソッド
+    public override string ExecuteMain()
+    {
+        // ForwardPlayerでライブラリ的に使用するので
+        // 実装今のところ不要
+        return "Completed";
+    }
+}
