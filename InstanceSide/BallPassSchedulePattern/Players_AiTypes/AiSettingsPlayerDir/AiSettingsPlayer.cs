9fc4108aac81625a6020f5ed70484892dc2fe1d3
Masarina <50221377+masarina@users.noreply.github.com>
2024-12-06 06:37:57 +0900

    Update AiSettingsPlayer.cs

--- InstanceSide/BallPassSchedulePattern/Players_AiTypes/AiSettingsPlayerDir/AiSettingsPlayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/AiSettingsPlayerDir/AiSettingsPlayer.cs
@@ -1,1 +1,30 @@
+using UdonSharp;
+using UnityEngine;
+
+public class AiSettingsPlayer : SuperPlayer
+{
+    public string myName;
+    
+    // 初期化メソッド (Pythonの__init__に相当)
+    public bool AiSettingsPlayerReset()
+    {
+        myName = "AiSettingsPlayer";
+        return true;
+    }
+
+    // プレイヤーの名前を返すメソッド
+    public override string ReturnMyName()
+    {
+        return "AiSettingsPlayer";
+    }
+
+    
+    // メイン処理を行うメソッド
+    public override string ExecuteMain()
+    {
+        // ForwardPlayerでライブラリ的に使用するので
+        // 実装今のところ不要
+        return "Completed";
+    }
+}
 
