8bf2eb786b3197755c4073323035e6f5b5358e63
Masarina <50221377+masarina@users.noreply.github.com>
2024-12-06 04:32:01 +0900

    Update ParamsPlayer.cs

--- InstanceSide/BallPassSchedulePattern/Players_AiTypes/ParamsPlayerDir/ParamsPlayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/ParamsPlayerDir/ParamsPlayer.cs
@@ -6,7 +6,10 @@ public class ParamsPlayer : SuperPlayer
     public RinaNumpy rinaNumpy;
     public string myName;
     public InitAiTypesPlayer initAiTypesPlayer;
-    int[] Shape = {XSize, LayerSize,}
+    int XSize = initAiTypesPlayer.XSize; //仮
+    int LayerSize = initAiTypesPlayer.LayerSize; //仮
+    int ParamsSize = initAiTypesPlayer.ParamsSize; // 重み、バイアス、ベータ値であれば、3とかかな。
+    int[] Shape = {XSize, LayerSize, ParamsSize}
     public float[][][][] = rinaNumpy.
     
     // 初期化メソッド (Pythonの__init__に相当)
