c7a0339706d378c4d08c54b0e738e58ccb613ac8
Masarina <50221377+masarina@users.noreply.github.com>
2024-12-13 05:04:17 +0900

    Update NormalizationPlayer.cs

--- InstanceSide/BallPassSchedulePattern/Players_AiTypes/NormalizationPlayerDir/NormalizationPlayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/NormalizationPlayerDir/NormalizationPlayer.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 public class NormalizationPlayer : SuperPlayer
 {
     public string myName;
+    
     public NormalizationLayer normalizationLayer;
     public InitAiTypesPlayer initAiTypesPlayer;
+    public SkipAddPlayer skipAddPlayer;
+    public AiFlagsPlayer aiFlagsPlayer;
+   
+    public AiSettingsPlayer aiSettingsPlayer;
+    public RinaNumpy rinaNumpy;
+    
+    public float[] x; // 順入力
+    public float[] y; // 順出力
+    public float[] dout; // 逆入力
+    public float[] dx; // 逆出力
+
+    
     
     // 初期化メソッド (Pythonの__init__に相当)
     public bool normalizationLayerReset()
@@ -22,23 +35,43 @@ public class NormalizationPlayer : SuperPlayer
 
     public float[] Forward(float[] x)
     {
-        float[] y = normalizationLayer.Forward(x);
+        y = normalizationLayer.Forward(x);
         
         return y;
     }
 
-    public float[] Backward(float[] dx)
+    public float[] Backward(float[] dout)
     {
-        float[] dout = normalizationLayer.Backward(dx)
+        float[] dx = normalizationLayer.Backward(dx)
 
-        return dout;
+        return dx;
     }
     
     // メイン処理を行うメソッド
     public override string ExecuteMain()
     {
-        // ForwardPlayerでライブラリ的に使用するので
-        // 実装今のところ不要
+
+        if (initAiTypesPlayer. TravelMode == "Forward")
+        {
+            // 1つ前のレイヤから、xを取ってくる
+            this.x = embeddingPlayer.SampleVecVer[aiFlagsPlayer.NowPositionIndex]
+        
+            // Forward処理
+            this.y = normalizationLayer.Forward(this.x)
+        }
+        else if (initAiTypesPlayer. TravelMode == "Backward")
+        {
+            // ひとつ先のLayerから、doutを取ってくる
+            this.dout = skipAddPlayer.dx
+        
+            // backward
+            this.dx = normalizationLayer.Backward(this.dout)
+            
+            // このポジション終了したので+=1
+            aiFlagsPlayer.NowPositionIndex += 1;
+        }
+        
+
         return "Completed";
     }
 }
