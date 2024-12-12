a307b6c10f04172e9badecebf0e25d4b8c024872
Masarina <50221377+masarina@users.noreply.github.com>
2024-12-12 21:50:05 +0900

    Create AiFlagsPlayer.cs

---
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/AiFlagsPlayerDir/AiFlagsPlayer.cs
@@ -0,0 +1,42 @@
+// 今学習しているポジションの場所等を
+// 保持する必要がある。
+// そのような保持が必要なものを
+// ここに定義することにした。
+
+// EmbeddingLayerのみ、パラメータは自分で持たせてOK.
+// Forward,Backwardの入力は、ベクトルを想定します。
+
+
+using UdonSharp;
+using UnityEngine;
+
+public class AiFlagsPlayer : SuperPlayer
+{
+    public string myName;
+
+    
+    // 初期化メソッド (Pythonの__init__に相当)
+    public bool AiFlagsPlayerReset()
+    {
+
+        myName = "EmbeddingPlayer";
+        return true;
+    }
+
+    // プレイヤーの名前を返すメソッド
+    public override string ReturnMyName()
+    {
+        return "AiFlagsPlayer";
+    }
+
+    
+    // メイン処理を行うメソッド
+    public override string ExecuteMain()
+    {
+        
+        
+        return "Completed";
+    }
+    
+    
+}
