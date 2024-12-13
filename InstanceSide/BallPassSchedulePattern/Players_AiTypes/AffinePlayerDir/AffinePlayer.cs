7920f7b7cef3a02f3d9269c02e722ef9803c1582
Masarina <50221377+masarina@users.noreply.github.com>
2024-12-14 02:00:14 +0900

    Update AffinePlayer.cs

--- InstanceSide/BallPassSchedulePattern/Players_AiTypes/AffinePlayerDir/AffinePlayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/AffinePlayerDir/AffinePlayer.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 public class AffinePlayer : SuperPlayer
 {
     public string myName;
+    
     public AffineLayer affineLayer;
     public InitAiTypesPlayer initAiTypesPlayer;
+    public AiFlagsPlayer aiFlagsPlayer;
+    public SkipAddPlayer skipAddPlayer;
+    
+    public float[] x;
+    public float[] y;
+    public float[] dout;
+    public float[] dx;
     
     // 初期化メソッド (Pythonの__init__に相当)
     public bool AffinePlayerReset()
@@ -27,18 +35,30 @@ public class AffinePlayer : SuperPlayer
         return y;
     }
 
-    public float[] Backward(float[] dx)
+    public float[] Backward(float[] dout)
     {
-        float[] dout = affineLayer.Backward(dx)
+        float[] dx = affineLayer.Backward(dout)
 
-        return dout;
+        return dx;
     }
     
     // メイン処理を行うメソッド
     public override string ExecuteMain()
     {
-        // ForwardPlayerでライブラリ的に使用するので
-        // 実装今のところ不要
+        if (aiFlagsPlayer.TravelMode == "Forward")
+        {
+            this.y = this.Forward(
+                skipAddPlayer.y
+            );
+            
+        }
+        else if
+        {
+            this.dc = this.Backward(
+                normalizationPlayer2.dx
+            );
+        }
+
         return "Completed";
     }
 }
