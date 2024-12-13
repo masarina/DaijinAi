f8d8903a95ceb8cb3ca654c7e66b66f7923c2397
Masarina <50221377+masarina@users.noreply.github.com>
2024-12-13 23:45:37 +0900

    Update SkipAddLayer.cs

--- InstanceSide/BallPassSchedulePattern/Players_AiTypes/SkipAddPlayerDir/SkipAddLayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/SkipAddPlayerDir/SkipAddLayer.cs
@@ -9,23 +9,16 @@ using UnityEngine;
 public class SkipAddLayer : UdonSharpBehaviour
 {
     // SkipAddLayerは、2つの入力配列を受け取って、それらを要素ごとに加算するシンプルなレイヤです。
-    public float[] Forward(float[] x1, float[] x2)
+    public float[] Forward(float[] x)
     {
-        if (x1.Length != x2.Length) throw new System.ArgumentException("Input arrays must be of equal length.");
-
-        float[] outArray = new float[x1.Length];
-        for (int i = 0; i < x1.Length; i++)
-        {
-            outArray[i] = x1[i] + x2[i];
-        }
-        return outArray;
+        return x; // 先のLayerで参照して、自身でaddさせる。
     }
 
     // Backwardメソッドでは、加算レイヤなので、受け取った勾配をそのまま前の層に伝えます。
-    public float[][] Backward(float[] dout)
+    public float[] Backward(float[] dout, float[] dout2)
     {
-        // doutは外部から受け取った勾配
-        // SkipAddLayerは加算を行うだけなので、勾配をそのまま前の層に伝える
+        rinaNumpy.Add_FloatArray_FloatArray(dout, dout2)
+
         return new float[][] { dout, dout };
     }
 }
