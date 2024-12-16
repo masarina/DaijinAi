9544474a50819ea8c61827751d0e377a388cc767
Masarina <50221377+masarina@users.noreply.github.com>
2024-12-17 03:26:18 +0900

    ParamsPlayer.cs を更新

--- InstanceSide/BallPassSchedulePattern/Players_AiTypes/ParamsPlayerDir/ParamsPlayer.cs
+++ InstanceSide/BallPassSchedulePattern/Players_AiTypes/ParamsPlayerDir/ParamsPlayer.cs
@@ -11,14 +11,13 @@ public class ParamsPlayer : SuperPlayer
     public int XSize;
     public int NumberOfAllLayers; // 全てのレイヤーの数
 
-
     // 初期化メソッド (Pythonの__init__に相当)
     public bool ParamsPlayerReset()
     {
         myName = "ParamsPlayer";
         
-        XSize = aiSettingsPlayer.XSize
-        
+        // aiSettingsPlayerからXSizeを取得
+        XSize = aiSettingsPlayer.XSize;
         
         return true;
     }
@@ -30,38 +29,39 @@ public class ParamsPlayer : SuperPlayer
     }
 
     public int[] ResultIndexOfParamsSave(
-                                int x, 
-                                int[] LayerIndex // EmbeddingLayerから数えて自分が何層目のLayerなのか。
-                )
+        int x, 
+        int LayerIndex // EmbeddingLayerから数えて自分が何層目のLayerなのか。
+    )
     // 引数：処理中のposition数
     // 戻値：このポジションが保存すべきParamsのIndexs
     {
-        // 結果を格納する配列を初期化
-        int[] y = new int[3];
-        int[] bs = { 0 + LayerIndex, 
-                1 + LayerIndex, 
-                2 + LayerIndex
-            };
+        // 結果を格納するリストを初期化
+        int[] y = new int[0]; // 空の配列として初期化
 
-        // 傾きは、
-        // (1Layerに3つ(今回)の空パラメータ付与) * (Embedding以降の全Layerの数)
+        // 保存インデックスを作成
+        int[] bs = new int[]
+        {
+            0 + LayerIndex,
+            1 + LayerIndex,
+            2 + LayerIndex
+        };
+
+        // 傾き (1Layerに3つの空パラメータ付与) * (Embedding以降の全Layerの数)
         int a = aiSettingsPlayer.LayerParamsSize * aiSettingsPlayer.LayerSize;
 
-        for (int bi = 0; bi < bs.Length;bi++)
+        // 保存インデックスを計算して格納
+        for (int bi = 0; bi < bs.Length; bi++)
         {
-            y = rinaNumpy.Append_FloatArray(y, a * x + bs[bi]);
+            y = rinaNumpy.Append_IntArray(y, a * x + bs[bi]); // 型に合わせてメソッドを変更
         }
 
         return y;
     }
-    
 
-    
     // メイン処理を行うメソッド
     public override string ExecuteMain()
     {
-
-
+        // 必要な処理を記述する
         return "Completed";
     }
 }
